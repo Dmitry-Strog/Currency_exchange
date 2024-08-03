2e86b3d1e852caefcf849e30615751c857a9a944
Dima <strogansev.dima@yandex.ru>
2024-08-03 13:40:36 +0300

    Добавлен Post-Запрос конкретного обменного курса

--- controller/handler_exchange_rates.py
+++ controller/handler_exchange_rates.py
@@ -1,6 +1,7 @@
 from controller.base_handler import BaseHandler
 from dao.exchange_dao import ExchangeRatesDAO
-from exception import MissingFormField, DatabaseUnavailableException
+from dto.exchange_dto import ExchangeDTO
+from exception import MissingFormField, DatabaseUnavailableException, CurrencyPairMissingError
 
 
 class ExchangeHandler(BaseHandler):
@@ -15,8 +16,18 @@ class ExchangeHandler(BaseHandler):
             return 500, {"message": error.message}
 
     def do_POST(self, data: dict):
-        base = data.get('baseCurrencyCode')
-        target = data.get('targetCurrencyCode')
-        rate = data.get('rate')
-        if base is None or target is None or rate is None:
-            raise MissingFormField
+        try:
+            base = data.get('baseCurrencyCode')
+            target = data.get('targetCurrencyCode')
+            rate = data.get('rate')
+            if base is None or target is None or rate is None:
+                raise MissingFormField
+            base_target_code: tuple = base, target
+            dto = ExchangeDTO(None, base, target, rate)
+            self.dao.save_exchange_currency(dto)
+            currency_dicts = self.dao.get_exchange(base_target_code).to_dict()
+            return 201, currency_dicts
+        except CurrencyPairMissingError as error:
+            return 409, {"message": error.message}
+        except MissingFormField as error:
+            return 400, {"message": error.message}
--- dao/exchange_dao.py
+++ dao/exchange_dao.py
@@ -40,26 +40,40 @@ class ExchangeRatesDAO:
         except IndexError:
             raise ExchangeRateNotFoundError
 
-    # def save_currency(self, dto: ExchangeDTO):
-    #     baseCurrencyCode, targetCurrencyCode, rate = dto.to_dict().values()
-    #     base = self.get_currency(baseCurrencyCode).id
-    #     target = self.get_currency(targetCurrencyCode).id
-    #     try:
-    #         with sqlite3.connect('exchange.db') as db:
-    #             cur = db.cursor()
-    #             query = "INSERT INTO exchangerates (base_currency_id, target_currency_id, rate) VALUES(?,?,?)"
-    #             cur.execute(query, (base, target, rate))
-    #             db.commit()
-    #     except sqlite3.IntegrityError:
-    #         raise ExchangeCodeAlreadyExistsError
+    def save_exchange_currency(self, dto: ExchangeDTO):
+        id, baseCurrencyCode, targetCurrencyCode, rate = dto.to_dict().values()
+        base = self.get_currency_by_code(baseCurrencyCode).id
+        target = self.get_currency_by_code(targetCurrencyCode).id
+        try:
+            with sqlite3.connect('exchange.db') as db:
+                cur = db.cursor()
+                query = "INSERT INTO exchangerates (base_currency_id, target_currency_id, rate) VALUES(?,?,?)"
+                cur.execute(query, (base, target, rate))
+                db.commit()
+        except sqlite3.IntegrityError:
+            raise ExchangeCodeAlreadyExistsError
 
     def get_currency_by_id(self, id):
-        """" Получение валюты из БД """
+        """" Получение валюты по id из БД """
         try:
             with sqlite3.connect('exchange.db') as db:
                 cur = db.cursor()
                 query = cur.execute(f"SELECT * FROM currencies WHERE id = '{id}'").fetchall()
                 currency_dto = CurrencyDTO(query[0][0], query[0][1], query[0][2], query[0][3])
+                return currency_dto
+        except sqlite3.OperationalError:
+            raise DatabaseUnavailableException
+        except IndexError:
+            raise CurrencyNotFoundError
+
+    def get_currency_by_code(self, code):
+        """" Получение валюты по code из БД """
+        try:
+            with sqlite3.connect('exchange.db') as db:
+                cur = db.cursor()
+                query = cur.execute(f"SELECT * FROM currencies WHERE code = '{code}'").fetchall()
+                currency_dto = CurrencyDTO(query[0][0], query[0][1], query[0][2], query[0][3])
+                print(query, code)
                 return currency_dto
         except sqlite3.OperationalError:
             raise DatabaseUnavailableException
